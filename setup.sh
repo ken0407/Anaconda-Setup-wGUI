a493f351b5e7dffedb6bf466feb1a442957b299a
kentaro <kentaro.ishii0407@gmail.com>
2019-02-16 11:04:26 +0900

    test

--- setup.sh
+++ setup.sh
@@ -12,14 +12,20 @@ eval $comment2
 comment3="sh Anaconda3-2018.12-Linux-x86_64.sh"
 eval $comment3
 
-command4="y"
+command4="sudo apt-get install gnome-core"
 eval $command4
 
-command5="n"
+command5="sudo apt-get install gnome-panel"
 eval $command5
 
-#command6="piyopiyo" #vncserver関連
-#eval $command6
+command6="sudo apt-get install vnc4server"
+eval $command6
 
-#command7="mv config.txt ~/.bashrc"
-#eval $command7
+command7="vncserver"
+eval $command7
+
+command8="vncserver -kill:1"
+eval $command8
+
+command9="mv config.txt ~/.vnc/xstartup"
+eval $command9
